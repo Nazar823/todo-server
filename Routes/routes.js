18a64d8c0878a86e4aef75678691554d57bb19f9
NazarExceedTeam <ignatenko.exceedteam@gmail.com>
2021-08-02 09:36:05 +0300

    orange

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,13 +1,13 @@
 const Router = require('express')
 const router = new Router
-const controller = require('./authController')
+const controller = require('../authController')
 
 
 const {
     registration,
     login,
     getUsers
-} = require('./authController')
+} = require('../authController')
 
 router.post('/registration', registration)
 router.post('/login', login)
